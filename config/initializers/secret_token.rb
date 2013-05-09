ae933497322bf7ddf1a3c765d2220aeabd033dec
Betsy Hauser <betsyhauser@gmail.com>
2013-05-09 16:44:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillshare::Application.config.secret_token = '8691166c1cf0b149e6142cb937c31b93b808095af397c0e1a2c4c599964e3f6c67d4fe14c5b14f83ac7207b9fdfdb3207c793ae3ef2446df337f03b385920f11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Skillshare::Application.config.session_store :cookie_store, key: '_skillshare_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Skillshare::Application.config.session_store :active_record_store
